4512e0e32c916ea445f49439f4d205e598c68fba
Ulises <uagonzal@ucsc.edu>
2024-02-05 02:02:46 -0800

    up to rocket.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ let config = {
 
 let game = new Phaser.Game(config)
 
+//reserve keyboard settings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
+
 //set UI sizes
 let borderUISize = game.config.height / 15
 let borderPadding = borderUISize / 3
